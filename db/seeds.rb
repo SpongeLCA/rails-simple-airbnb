498877e7b3b8a449bef702197051621611e74b4d
Loic CARPENTIER <carpentier.loic19@gmail.com>
2024-07-03 11:23:47 +0200

    fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+
+4.times do
+  Flat.create!(
+    name: Faker::Lorem.sentence(word_count: 3),
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 5),
+    price_per_night: rand(50..300),
+    number_of_guests: rand(1..10),
+    picture_url: "https://loremflickr.com/320/240/house"
+  )
+end
+
+puts 'Finished!'
